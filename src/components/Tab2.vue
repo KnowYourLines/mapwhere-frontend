c5b818a577d981ccfc72765e8010bc0f42108ec3
Johnny Li <jonathan.li@limejump.com>
2021-08-17 04:26:48 +0100

    transport mode and time input created

--- src/components/Tab2.vue
+++ src/components/Tab2.vue
@@ -1,18 +1,66 @@
 <template>
   <div>
-    <input
-      ref="input"
-      class="controls"
-      type="text"
-      :value="yourLocation"
-      placeholder="Enter location"
-      @focus="$event.target.select()"
-    /><br /><br /><img
+    <br />
+    <img
       src="@/assets/icons8-location-off-50.png"
       @click="getCurrentLocation"
       class="tab-button"
       width="25"
-    />
+    /><br />
+    <input
+      ref="input"
+      class="controls"
+      type="text"
+      v-model="yourLocation"
+      placeholder="Enter start location"
+      @focus="$event.target.select()"
+    /><br /><br />
+    <div>
+      <img
+        src="@/assets/icons8-public-transportation-50.png"
+        v-bind:class="{ active: travelMode === 'transit' }"
+        @click="travelMode = 'transit'"
+        class="tab-button"
+        width="25"
+      /><img
+        src="@/assets/icons8-walking-50.png"
+        v-bind:class="{ active: travelMode === 'walk' }"
+        @click="travelMode = 'walk'"
+        class="tab-button"
+        width="25"
+      /><img
+        src="@/assets/icons8-cycling-100.png"
+        v-bind:class="{ active: travelMode === 'bike' }"
+        @click="travelMode = 'bike'"
+        class="tab-button"
+        width="25"
+      />
+      <img
+        src="@/assets/icons8-car-60.png"
+        v-bind:class="{ active: travelMode === 'car' }"
+        @click="travelMode = 'car'"
+        class="tab-button"
+        width="25"
+      /><br />
+      <div id="time-form">
+        <input
+          type="number"
+          v-model="hoursToTravel"
+          min="0"
+          @focus="$event.target.select()"
+        /><label> hours </label>
+        <input
+          type="number"
+          min="0"
+          v-model="minutesToTravel"
+          @focus="$event.target.select()"
+        /><label> minutes</label>
+      </div>
+      <br />
+      <button type="button" class="btn" @click="updateIsochrone">
+        Find reachable area
+      </button>
+    </div>
     <div v-if="noLocationFound"><br />No location found.</div>
     <div v-if="possiblePlaces">
       <ul id="array-rendering">
@@ -39,9 +87,15 @@ export default {
       possiblePlaces: null,
       yourLocation: null,
       noLocationFound: false,
+      travelMode: "transit",
+      hoursToTravel: 0,
+      minutesToTravel: 0,
     };
   },
   methods: {
+    updateIsochrone: function () {
+      console.log("send something over the websocket");
+    },
     selectLocation: function (place) {
       this.lat = place.geometry.location.lat();
       this.lng = place.geometry.location.lng();
@@ -110,5 +164,27 @@ export default {
 <style >
 #array-rendering {
   list-style-type: none;
+}
+.tab-button {
+  padding: 6px 10px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  background: #f0f0f0;
+  margin-bottom: -1px;
+  margin-right: -1px;
+}
+.tab-button:hover {
+  background: #e0e0e0;
+}
+.tab-button.active {
+  background: #e0e0e0;
+}
+#time-form {
+  white-space: pre;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
