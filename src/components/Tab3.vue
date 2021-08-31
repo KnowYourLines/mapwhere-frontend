e5a6be5020c8ee0d67188b7b945fb5fac4cb94ee
Johnny Li <jonathan.li@limejump.com>
2021-08-31 10:41:48 +0100

    added poi type selection

--- src/components/Tab3.vue
+++ src/components/Tab3.vue
@@ -9,6 +9,19 @@
         </li>
       </ul>
     </div>
+    <div v-if="!missingArea" id="v-model-select-dynamic" class="demo">
+      <br /><span>Selected: {{ selected }}</span
+      ><br />
+      <select v-model="selected">
+        <option
+          v-for="option in options"
+          :value="option.value"
+          :key="option.value"
+        >
+          {{ option.text }}
+        </option>
+      </select>
+    </div>
     <div v-if="!missingArea" id="map" ref="map"></div>
     <div v-else ref="noArea">No area found. Try to travel further.</div>
   </div>
@@ -35,6 +48,105 @@ export default {
     return {
       missingArea: false,
       missingLocations: false,
+      selected: "",
+      options: [
+        { text: "Accounting", value: "accounting" },
+        { text: "Airport", value: "airport" },
+        { text: "Amusement Park", value: "amusement_park" },
+        { text: "Aquarium", value: "aquarium" },
+        { text: "Art Gallery", value: "art_gallery" },
+        { text: "ATM", value: "atm" },
+        { text: "Bakery", value: "bakery" },
+        { text: "Bank", value: "bank" },
+        { text: "Bar", value: "bar" },
+        { text: "Beauty Salon", value: "beauty_salon" },
+        { text: "Bicycle Store", value: "bicycle_store" },
+        { text: "Book Store", value: "book_store" },
+        { text: "Bowling Alley", value: "bowling_alley" },
+        { text: "Bus Station", value: "bus_station" },
+        { text: "Cafe", value: "cafe" },
+        { text: "Campground", value: "campground" },
+        { text: "Car Dealer", value: "car_dealer" },
+        { text: "Car Rental", value: "car_rental" },
+        { text: "Car Repair", value: "car_repair" },
+        { text: "Car Wash", value: "car_wash" },
+        { text: "Casino", value: "casino" },
+        { text: "Cemetery", value: "cemetery" },
+        { text: "Church", value: "church" },
+        { text: "City Hall", value: "city_hall" },
+        { text: "Clothing Store", value: "clothing_store" },
+        { text: "Convenience Store", value: "convenience_store" },
+        { text: "Courthouse", value: "courthouse" },
+        { text: "Dentist", value: "dentist" },
+        { text: "Department Store", value: "department_store" },
+        { text: "Doctor", value: "doctor" },
+        { text: "Drugstore", value: "drugstore" },
+        { text: "Electrician", value: "electrician" },
+        { text: "Electronics Store", value: "electronics_store" },
+        { text: "Embassy", value: "embassy" },
+        { text: "Fire Station", value: "fire_station" },
+        { text: "Florist", value: "florist" },
+        { text: "Funeral Home", value: "funeral_home" },
+        { text: "Furniture Store", value: "furniture_store" },
+        { text: "Gas Station", value: "gas_station" },
+        { text: "Gym", value: "gym" },
+        { text: "Hair Care", value: "hair_care" },
+        { text: "Hardware Store", value: "hardware_store" },
+        { text: "Hindu Temple", value: "hindu_temple" },
+        { text: "Home Goods Store", value: "home_goods_store" },
+        { text: "Hospital", value: "hospital" },
+        { text: "Insurance Agency", value: "insurance_agency" },
+        { text: "Jewelry Store", value: "jewelry_store" },
+        { text: "Laundry", value: "laundry" },
+        { text: "Lawyer", value: "Lawyer" },
+        { text: "Library", value: "library" },
+        { text: "Light Rail Station", value: "light_rail_station" },
+        { text: "Liquor Store", value: "liquor_store" },
+        { text: "Local Government Office", value: "local_government_office" },
+        { text: "Locksmith", value: "locksmith" },
+        { text: "Lodging", value: "lodging" },
+        { text: "Meal_delivery", value: "meal_delivery" },
+        { text: "Meal Takeaway", value: "meal_takeaway" },
+        { text: "Mosque", value: "mosque" },
+        { text: "Movie Rental", value: "movie_rental" },
+        { text: "Movie Theater", value: "movie_theater" },
+        { text: "Moving Company", value: "moving_company" },
+        { text: "Museum", value: "museum" },
+        { text: "Night Club", value: "night_club" },
+        { text: "Painter", value: "painter" },
+        { text: "Park", value: "park" },
+        { text: "Parking", value: "parking" },
+        { text: "Pet Store", value: "pet_store" },
+        { text: "Pharmacy", value: "pharmacy" },
+        { text: "Physiotherapist", value: "physiotherapist" },
+        { text: "Plumber", value: "plumber" },
+        { text: "Police", value: "police" },
+        { text: "Post Office", value: "post_office" },
+        { text: "Primary School", value: "primary_school" },
+        { text: "Real Estate Agency", value: "real_estate_agency" },
+        { text: "Restaurant", value: "restaurant" },
+        { text: "Roofing Contractor", value: "roofing_contractor" },
+        { text: "RV Park", value: "rv_park" },
+        { text: "School", value: "school" },
+        { text: "Secondary School", value: "secondary_school" },
+        { text: "Shoe Store", value: "shoe_store" },
+        { text: "Shopping Mall", value: "shopping_mall" },
+        { text: "Spa", value: "spa" },
+        { text: "Stadium", value: "stadium" },
+        { text: "Storage", value: "storage" },
+        { text: "Store", value: "store" },
+        { text: "Subway Station", value: "subway_station" },
+        { text: "Supermarket", value: "supermarket" },
+        { text: "Synagogue", value: "synagogue" },
+        { text: "Taxi Stand", value: "taxi_stand" },
+        { text: "Tourist Attraction", value: "tourist_attraction" },
+        { text: "Train Station", value: "train_station" },
+        { text: "Transit Station", value: "transit_station" },
+        { text: "Travel Agency", value: "travel_agency" },
+        { text: "University", value: "university" },
+        { text: "Veterinary Care", value: "veterinary_care" },
+        { text: "Zoo", value: "zoo" },
+      ],
     };
   },
   beforeUpdate() {
