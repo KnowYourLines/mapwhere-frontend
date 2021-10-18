ac73e246986c5389cc359c310d98c9ea3317644b
Johnny <knowyourlines@outlook.com>
2021-10-19 00:06:08 +0100

    try unoptimized markers

--- src/components/Tab3.vue
+++ src/components/Tab3.vue
@@ -150,6 +150,7 @@ export default {
           this.markers[i] = new window.google.maps.Marker({
             position: this.placeResults[i].geometry.location,
             animation: window.google.maps.Animation.DROP,
+            optimized: false
           });
           // If the user clicks a marker, show the details in an info window.
           this.markers[i].placeResult = this.placeResults[i];
@@ -356,6 +357,7 @@ export default {
           this.markers[i] = new window.google.maps.Marker({
             position: this.placeResults[i].geometry.location,
             animation: window.google.maps.Animation.DROP,
+            optimized: false
           });
           // If the user clicks a marker, show the details in an info window.
           this.markers[i].placeResult = this.placeResults[i];
